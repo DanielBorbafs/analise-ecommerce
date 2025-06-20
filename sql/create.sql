30feec3674bdbd87a763176fc415e2921788e69e
DanielBorbafs <dev.danielborba@gmail.com>
2025-06-20 20:59:28 -0300

    criando banco e carregando tabela de clientes

---
+++ sql/create.sql
@@ -0,0 +1,11 @@
+
+create table customers(
+customer_id varchar(100) primary key,
+customer_unique_id varchar(50),
+customer_zip_code_prefix varchar(50),
+customer_city varchar(50),
+customer_state char(2)
+);
+go
+
+
